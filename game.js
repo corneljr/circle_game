36a87bf4bdb2a7fcab721eae57ddf989eeecd6df
Josh Cornelius <joshcornelius9@gmail.com>
2014-07-08 15:12:51 -0400

    finished game

--- game.js
+++ game.js
@@ -1,33 +1,33 @@
-function Game(circleCount, duration) {
-	this.score = 0;
-	this.circles = [];
+function Game(duration, circleCount) {
+	this.score = 0
 	this.duration = (duration || 10) * 1000;
-
+	this.circles = [];
 
 	this.start = function() {
-		for (var i = 0; i < 10; i++) {
+		for (var i = 0; i < circleCount; i++) {
 			this.circles.push(new Circle());
 			this.circles[i].render();
 			this.circles[i].move();
 		}
-
-		$('#score').text(this.score);
-		setTimeout(this.stop, this.duration);
+		$('.score span').text(window.game.score)
+		setTimeout(this.stop, this.duration)
 	}
 
 	this.stop = function() {
-		alert('GAME OVER!');
-		for (var i = 0; i < this.circleCount; i++) {
-			game.circles[i].$me.remove();
-		}
+		$('.circle').fadeOut(400);
+		$('.game-end').fadeIn(1000);
+		$('.game-end-text').text('Score: ' + window.game.score + ' (click to play again)')
+		$('.game-end').on('click', function(){
+			setTimeout(
+				function(){ location.reload() }, 3000)
+		});
 	}
 }
 
 function Circle() {
-	this.x = Math.random() * 420;
-	this.y = Math.random() * 420;
-	this.diameter = 30 + Math.random() * 50;
-	this.speed = 500 + Math.random() * 1500;
+	this.diameter = 30 + Math.random() * 50
+	this.x = Math.random() * 420
+	this.y = Math.random() * 420
 
 	this.render = function() {
 		var _this = this
@@ -44,36 +44,30 @@ function Circle() {
 	};
 
 	this.move = function() {
-		var _this = this;
+		var _this = this
 		this.$me.animate({
-		left: Math.random() * 420,
-		top: Math.random() * 420
-	}, {
-		duration: this.speed,
-		complete: function() {
-			_this.move();
-			}
+			left: Math.random() * 420,
+			top: Math.random() * 420
+		}, Math.random() * 2000, function () { 
+			_this.move()
 		});
-	};
+	}
 
 	this.kill = function() {
-    this.$me.css('background-color', 'red')
-      .effect({
-        effect: 'explode',
-        duration: 100,
-        complete: function() {
-          $(this).remove();
-          $("#score").text(window.game.score += 100);
-        },
-        queue: false
-    });
-  };
+		this.$me.css('background-color', 'red')
+			.hide('explode', {
+				queue: false,
+				complete: function() {
+					$(this).remove();
+				}
+		});
+		console.log(this.diameter)
+		window.game.score += (80 - Math.floor(this.diameter)) * 100
+		$('.score span').text(window.game.score)
+	}
 }
 
-
 $(document).ready( function() {
-	var game = new Game(10,10)
+	game = new Game(10,10);
 	game.start();
-
 });
-
